b43d309bad60bd84ba6fde7c1355ad1506fc37cb
progahmedbasha <40414145+progahmedbasha@users.noreply.github.com>
2020-11-09 01:20:39 +0200

    Update ProductsController.php

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -8,16 +8,34 @@ use App\Product;
 use App\Category;
 
 
+
 class ProductsController extends Controller
 {
     public function products_show()
     {
     	
     	$data = Product::all();
+        $x = DB::table('categories')->get();
 
-    	return view('products', compact('data'));
+    	return view('products', compact('data', 'x'));
 
     }
+    public function products_store(Request $request)
+    {
+            $product = new Product;
+        $product->product_name =$request->product_name;
+        $product->product_price=$request->product_price;
+        $product->qty=$request->qty;
+        $product->category_id=$request->category_id;
+
+
+        $product->save();
+        return back();
+    }
+
+
+
+
         public function oneproduct(Category $product)
     {
     	    	$data = DB::table('products')->get();
@@ -36,5 +54,14 @@ class ProductsController extends Controller
 
     	return view('categories',compact('data'));
 
+    }
+
+     public function categories_store(Request $request)
+    {
+            $category = new Category;
+        $category->title =$request->title;
+
+        $category->save();
+        return back();
     }
 }
